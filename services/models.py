882785bd4fad836e22dc9c93be3693a1e6839397
ALVIN MARCO CRISTY TAMPUBOLON <alvinmarco8@gmail.com>
2026-02-02 21:07:28 +0700

    day 2

--- services/models.py
+++ services/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.PositiveIntegerField(
+        help_text="Duration in minutes"
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.duration} min)"
